8e5d49b046786e3daca6003b3054778f6ae2fdb0
clarktyra <clark.tyra@gmail.com>
2019-04-08 09:09:33 -0700

    installed babel

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,13 @@
-console.log('App.js is running')
+'use strict';
+
+console.log('App.js is running');
 
 // JSX - JavaScript XML
-//var template = <p>This is JSX from app.js!</p>;
-var template = React.createElement("h1", {
-    id: "someID"
-  }, "Something New");
-var appRoot = document.getElementById('app')
+var template = React.createElement(
+  'h1',
+  null,
+  'Indecision App'
+);
+var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
